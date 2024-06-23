fa521b8ffd6c531d77fbb4dcf544250f40d94f70
Arceus <harshayerraguntla81109181@gmail.com>
2024-06-23 10:37:07 +0530

    My First Commit

---
+++ API/Controllers/UserController.cs
@@ -0,0 +1,30 @@
+using API.Data;
+using API.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+namespace API.Controller;
+
+
+[ApiController]
+[Route("api/[controller]")]
+public class UserController : ControllerBase
+{
+    private readonly DataContext dataContext;
+    public UserController(DataContext Data)
+    {
+        this.dataContext = Data;
+    }
+
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<UserData>>> Getdata()
+    {
+        var users = await dataContext.Users.ToListAsync();
+        return users;
+    }
+
+    [HttpGet("{id}")]
+    public async Task<ActionResult<UserData>> GetDataasperID(int id)
+    {
+        return dataContext.Users.Find(id);
+    }
+}
---
+++ API/Entities/UserData.cs
@@ -0,0 +1,6 @@
+namespace API.Entities;
+
+public class UserData{
+    public int ID { get; set; }
+    public string UserName { get; set; }
+}
